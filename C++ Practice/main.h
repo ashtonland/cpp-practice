7ffb617fd0e553dc232eb248fafd041983cced82
Ashton <65512990+Treixatek@users.noreply.github.com>
2023-04-10 12:09:14 -0400

    Notes for the header file

--- C++ Practice/main.h
+++ C++ Practice/main.h
@@ -8,15 +8,26 @@ public:
 	int money;
 
 public:
+	//these are the 2 overloads for the constructor
 	Military(int troops, int money);
 	Military();
+
+	// define the function return as const because it does not
+	// actually modify any variable of the class, only returns values
 	int GetTroopCount() const { return reserveCount + mobalizingCount + activeCount; }
+	
+	// functions are declared in, but defined in a cpp file
 	void Mobalize(int count);
 };
 
+// this class inherites all the functions and vars of the Military
+// class, except the constructor
 class ArmorDivision : public Military {
 public:
+	//constructor for this subclass
 	ArmorDivision();
+
+	//add subclass specific functons, and overloads for references and pointers
 	bool Attack(Military& attackedForce);
 	bool Attack(Military* attackedForce);
 };
